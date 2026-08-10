6443d6fa040d25e85cd05825992c5aa357bf0735
Hammad Ali Khan <alikhanhammad394@gmail.com>
2026-08-10 12:59:04 +0500

    Had a basic introduction to JavaScript

---
+++ JS/Introduction/script.js
@@ -0,0 +1,25 @@
+alert("Hello World")
+
+console.log("Code is running")
+console.log("Code is also running")
+console.log("Code is looking like a WoW")
+
+var a = prompt("Enter Your Favourite Number:")
+
+console.log("Your favourite number is : " +a);
+
+
+var isTrue= confirm("Are you sure you want to blast your computer?");
+
+if(isTrue){
+    console.log("Computer is Blasting...")
+}
+else{
+        console.log("Computer is not Blasting...")
+}
+
+
+document.title = "Hey JavaScript"
+
+
+document.body.style.backgroundColor = "red"
