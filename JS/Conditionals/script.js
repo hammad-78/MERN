16b88044479bc66a882925975628ee708ce2a5b4
Hammad Ali Khan <alikhanhammad394@gmail.com>
2026-08-11 10:49:50 +0500

    Learned Condionals and operators in JS

---
+++ JS/Conditionals/script.js
@@ -0,0 +1,47 @@
+console.log("Hello World")
+
+
+var age = 45;
+ if(age>=18){
+    console.log("You can vote");
+ }
+ else{
+    console.log("You cannot vote");     
+ }
+
+// Operators in JavaScript
+
+let a = 3, b=2;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a%b);
+console.log(a**b);
+
+
+
+// Have done these things soo many times 
+
+// if-else laddar
+
+if(age>=18){
+    console.log("You can Drive");
+}
+else if(age == 0){
+    console.log("Are you kidding");
+}
+else{
+   console.log("You cannot drive");
+}
+
+
+
+// Ternary Operator
+
+let x = 3, y = 4;
+
+let z = (x>y)? (x-y) : (y-x);
+
+console.log("The vakue of Z is: " +z);
